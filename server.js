98d03b366315500bc9f2ac8c7107febc73aa44f2
pcion <patrick.ciongoli@gmail.com>
2022-05-31 00:37:13 -0400

    updated server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI =
+   process.env.MONGODB_URI || "mongodb://localhost:27017/pc-budget-tracker";
 
 const app = express();
 
@@ -15,13 +17,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-   process.env.MONGODB_URI || "mongodb://localhost:27017/pc-budget-tracker",
-   {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-   }
-);
+mongoose.connect(MONGODB_URI, {
+   useNewUrlParser: true,
+   useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
